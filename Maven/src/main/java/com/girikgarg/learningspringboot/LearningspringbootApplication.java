f94d197f4609e9adda51721aa370a1d205c9021f
Girik Garg <girik_garg@intuit.com>
2025-09-14 14:44:48 +0530

    feat: maven build lifecycle

---
+++ Maven/src/main/java/com/girikgarg/learningspringboot/LearningspringbootApplication.java
@@ -0,0 +1,16 @@
+package com.girikgarg.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class.
+ */
+@SpringBootApplication
+public class LearningspringbootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearningspringbootApplication.class, args);
+  }
+
+}
---
+++ Maven/src/test/java/com/girikgarg/learningspringboot/LearningspringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.girikgarg.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Test cases are running for my project");
+	}
+
+}
