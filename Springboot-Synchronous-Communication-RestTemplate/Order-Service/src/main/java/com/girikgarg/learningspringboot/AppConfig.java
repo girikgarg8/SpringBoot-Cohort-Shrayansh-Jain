17c568dddd6afb2744c09f32ec82374bab76ba71
Girik Garg <girik_garg@intuit.com>
2025-10-10 22:46:58 +0530

    fix: add http client request factory

--- Springboot-Synchronous-Communication-RestTemplate/Order-Service/src/main/java/com/girikgarg/learningspringboot/AppConfig.java
+++ Springboot-Synchronous-Communication-RestTemplate/Order-Service/src/main/java/com/girikgarg/learningspringboot/AppConfig.java
@@ -2,12 +2,19 @@ package com.girikgarg.learningspringboot;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setConnectTimeout(1000);
+        factory.setReadTimeout(5000);
+
         return new RestTemplate();
     }
+
 }
